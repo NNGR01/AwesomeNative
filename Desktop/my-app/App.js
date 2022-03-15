a5de5e5bc58c57883a784f0a6c51a1e370085b87
Nicolas Guzman Rivera <guzman.r.nicolas@gmail.com>
2022-03-14 22:44:51 -0300

    elfetch

--- Desktop/my-app/App.js
+++ Desktop/my-app/App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import React from 'react';
 import {
   StyleSheet,
   Text,
@@ -9,29 +10,35 @@ import {
   Platform,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Bar from "./Bar";
+import { Card } from "react-native-elements/dist/card/Card";
+
 
 const App = () => {
 
   const [pickImage, setPickImage] = useState(null);
 
+  
+ 
+  
+  
   const openImagePickerAsync = async () => {
     const permissionResult =  await ImagePicker.requestMediaLibraryPermissionsAsync()
- 
-
-  if(permissionResult.granted === false) {
-    alert('permissions to use an image are required');
-    return ;
+    
+    
+    if(permissionResult.granted === false) {
+      alert('permissions to use an image are required');
+      return ;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync()
+    
+    if (pickerResult.cancelled === true ){
+      return ;
+    }
+    setPickImage({localUri : pickerResult.uri})
+    
   }
-  const pickerResult = await ImagePicker.launchImageLibraryAsync()
-  
-  if (pickerResult.cancelled === true ){
-    return ;
-  }
-  setPickImage({localUri : pickerResult.uri})
-
-}
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Pick an Image!!</Text>
@@ -45,31 +52,49 @@ source={{uri : pickImage !== null ? pickImage.localUri : 'https://picsum.photos/
 />
 
 </TouchableOpacity>
-  
-
     <Bar />
+
+
     </View>
    
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#292929",
-    alignItems: "center",
-    justifyContent: "center",
-    color: "#fff",
-  },
-  photo: { height: 200, width: 200, borderRadius: 100,  },
-  title: { color: "#fff", fontSize: 30, marginBottom: 20 },
-  button: {
-    backgroundColor: "blue",
-    padding: 7,
-    marginTop: 10,
-  },
-  buttonText: { color: "#fff", fontSize: 20 },
+   );
+  };
   
-});
-
-export default App;
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#292929",
+      alignItems: "center",
+      justifyContent: "center",
+      color: "#fff",
+    },
+    photo: { height: 200, width: 200, borderRadius: 100,  },
+    title: { color: "#fff", fontSize: 30, marginBottom: 20 },
+    button: {
+      backgroundColor: "blue",
+      padding: 7,
+      marginTop: 10,
+    },
+    buttonText: { color: "#fff", fontSize: 20 },
+    
+  });
+  
+  export default App;
+  
+  /*   const arr = [];
+    useEffect(() => {
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=00")
+        .then((response) => response.json())
+        .then((data) =>
+          setResult(
+            data.results.map((item) => {
+              fetch(item.url)
+                .then((response) => response.json())
+                .then((allpokemon) => arr.push(allpokemon));
+              setPoke(arr);
+            })
+          )
+        );
+    }, []);
+   
+  console.log(arr); */
--- Desktop/my-app/Bar.js
+++ Desktop/my-app/Bar.js
@@ -1,23 +1,63 @@
-import { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { useState, useEffect } from "react";
+import { Text, View, StyleSheet, ScrollView,  TouchableOpacity, Image } from "react-native";
 import { SearchBar } from "react-native-elements";
 
-const Bar = () => {
+const Bar = props => {
+  const [pokemons, setPokemons] = useState([]);
   const [search, setSearch] = useState("");
 
-  const updateSearch = (search) => {
-    setSearch(search);
+  useEffect(() =>{
+    fetchPokemons();
+  },[])
+
+  const fetchPokemons = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=500')
+    .then(response => response.json())
+    .then(pokemons => setPokemons(pokemons.results));
   };
 
+
   return (
-      <View style={styles.seeker} >
-  
+    <View>
+      <View style={styles.box} >
       <SearchBar
         style={{margin : 0}}
-        placeholder="..."
-        onChangeText={updateSearch}
+        placeholder="Search Pokemons"
+        onChangeText={value => setSearch(value)}
         value={search}
       />
+    </View>
+
+<ScrollView>
+<View style={styles.seeker}>
+    {
+      pokemons
+            .filter(pokemon =>
+              pokemon.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((pokemon, index) => {
+              return (
+          <TouchableOpacity
+          activeOpacity={0.5}
+          key={index}
+          style={styles.box}
+          onPress={() => console.log('hello')
+          }>
+  <Image 
+    style={{width : 150 , height : 150}}
+    source={{
+      uri :
+   `https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${pokemon.name}.png`,
+    }}
+  />
+  <Text>{pokemon.name} </Text>
+          </TouchableOpacity>
+        )
+      })
+    }
+</View>
+</ScrollView>
+
     </View>
   );
 };
@@ -25,11 +65,29 @@ const Bar = () => {
   seeker: {
     marginTop : 20,
         width : '50%',
-        height : 25,
-        backgroundColor : 'white',
-        borderRadius : 8
+        display: 'flex',
+        height : 150,
+        justifyContent: 'center'
+        
+  }, 
+  box : {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  card: {
+    display: 'flex',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    marginHorizontal: 20,
+    marginVertical: 10,
   }
   
 }); 
 
 export default Bar;
+
+/* props.navigation.navigate('Details',{pokemon : pokemon.name} */
