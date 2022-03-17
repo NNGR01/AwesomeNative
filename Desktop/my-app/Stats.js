42d18cb07c7907c26f50236c15bb309644205e31
Nicolas Guzman Rivera <guzman.r.nicolas@gmail.com>
2022-03-16 23:35:41 -0300

    intentodestats

--- Desktop/my-app/Stats.js
+++ Desktop/my-app/Stats.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { View } from 'react-native';
 
-const Stats = () =>{
-    return (
+
+
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { Image } from 'react-native';
+import { Text, View } from 'react-native';
+
+
+const Stats = props => {
+
+    const [stats, setStats] = useState([]);
+    
+    const fetchPokemonsStats = () => {
+        const { id } = props.navigation;
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id.params.pokemons}`)
+        .then(res = res.json())
+        .then(stats => setStats(stats));
+    };
+    useEffect(() =>{
+        fetchPokemonsStats();
+    },[]);
+
+    
+    return stats.name ? (
         <View>
-            <Text>Hello</Text>
+            <Image 
+        style={{height : 100 , width : 100}}
+        source={{
+            uri : `https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${
+            details.name
+          }.png`,
+        }}
+            />
+            <Text> Name : {stats.name} </Text>
         </View>
+    ) : (
+        <View>
+        <ActivityIndicator size="large" color="#E63F34" />
+      </View>
     )
 };
-export default Stats;
+export default Stats; 
